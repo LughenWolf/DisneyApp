6c8524075ba5824f2b03a2ce058195fec7a33166
caco83100 <31388813+caco83100@users.noreply.github.com>
2026-03-13 09:40:22 +0100

    import

--- app/src/main/java/fr/isen/s8/LrMmMr/MainActivity.kt
+++ app/src/main/java/fr/isen/s8/LrMmMr/MainActivity.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
